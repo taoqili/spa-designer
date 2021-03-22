2eaa57b0efeadd9a895b11cad613e103acbc95e1
chemingjun <chemingjun975@hellobike.com>
2021-03-22 16:35:58 +0800

    add comlibs in examples

--- examples/config.tsx
+++ examples/config.tsx
@@ -2,13 +2,13 @@ import {getLocationSearch} from "./utils";
 import {LS_DEFAULT_KEY, LS_VB_PRE} from "./constants";
 
 export default {
-  comlibAdder(): Promise<any> {//Demo
-    return new Promise((resolve, reject): void => {
-      import('../../comlib-logic-normal').then(lib => {
-        resolve(lib.default)
-      })
-    })
-  },
+  // comlibAdder(): Promise<any> {//Demo
+  //   return new Promise((resolve, reject): void => {
+  //     import('../../comlib-logic-normal').then(lib => {
+  //       resolve(lib.default)
+  //     })
+  //   })
+  // },
   comlibLoader,
   pageLoader,
   stage: {//舞台
@@ -38,16 +38,8 @@ export default {
 
 function comlibLoader(): Promise<any> {
   return new Promise((resolve, reject): void => {
-    import('../../comlib-logic-normal').then(lib => {
+    import('./comlib').then(lib => {
       const libs = [lib.default]
-      try {
-        const chartLib = require('../../comlib-pc-normal')
-        if (chartLib) {
-          libs.push(chartLib.default)
-        }
-      } catch (e) {
-
-      }
       resolve(libs)
     })
   })
